f975a60599b7e372a58974f984418560c6c2dbf8
Mariusz Gezicki <mariusz.v7@gmail.com>
2014-12-31 10:27:45 +0100

    modified squad parser to put player id on top of stats skip already parsed files

--- parse_squads.py
+++ parse_squads.py
@@ -1,6 +1,4 @@
 import os
-#from HTMLParser import HTMLParser
-#from BeautifulSoup import BeautifulSoup
 from pyquery import PyQuery   
 
 def get_id(row):
@@ -17,7 +15,12 @@ def parse():
     squads = os.listdir("squad")
     for squad in squads:
         #driver.get("squad/"+squad+"/squad.html")
-        file_ = open("squad/"+squad+"/squad.html")
+        file_path = "squad/"+squad
+
+        if os.path.exists(file_path+"/squad.txt"):
+            continue
+
+        file_ = open(file_path+"/squad.html")
         html = file_.readlines()
 
         str_ = ""
@@ -27,25 +30,23 @@ def parse():
         parser = PyQuery(str_)
         head = parser('thead>tr>th')
         labels = []
+        labels.append('id')
         for label in head.items():
             labels.append(label.text())
-        ###
-        labels.append('id')
-
+        ##
         table = parser('tbody>tr')
         players = []
         i = 0
         for row in table.items():
             players.append([])
+            players[i].append(get_id(row))
             for arg in row('td').items():
                 #print arg.text()
                 players[i].append(arg.text().encode('UTF-8'))
-            
-            players[i].append(get_id(row))
             i += 1
 
 
-        file_ = open("errors/test.txt", "w")
+        file_ = open(file_path+"/squad.txt", "w")
         for player in players:
             i = 0
             for atr in player:
